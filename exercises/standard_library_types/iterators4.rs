cd22197802bcadae3b369af25a71d88a48dcc73c
Daniel Means <dmeans@cloudflare.com>
2023-01-06 20:48:14 -0800

    Iterators4 recursive implementation

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -12,6 +12,11 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if num == 0 {
+        return 1
+    } else {
+        return num * factorial(num - 1)
+    }
 }
 
 #[cfg(test)]
