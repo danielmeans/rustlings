2b492dfa41fa9266ab6f6aa6f5cc86490e5f45ed
Daniel Means <dmeans@cloudflare.com>
2023-01-09 22:02:13 -0800

    Threads WIP

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -11,18 +11,22 @@
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use std::pin::Pin;
+use std::ops::DerefMut;
 
+#[derive(DerefMut)]
 struct JobStatus {
     jobs_completed: u32,
 }
 
 fn main() {
     let status = Arc::new(JobStatus { jobs_completed: 0 });
-    let status_shared = status.clone();
+    let status_shared = Arc::clone(&status);
     thread::spawn(move || {
         for _ in 0..10 {
-            thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            thread::sleep(Duration::from_millis(250));  
+            let value = Pin::new(&mut status_shared.jobs_completed).get_mut();
+            *value += 1;
         }
     });
     while status.jobs_completed < 10 {
