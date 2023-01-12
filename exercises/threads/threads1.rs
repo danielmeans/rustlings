e7d3210fce00de732c48e37bdd961c79c78a71cf
Daniel Means <dmeans@cloudflare.com>
2023-01-11 21:27:19 -0800

    Threads execise completed

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,28 +6,26 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use std::pin::Pin;
-use std::ops::DerefMut;
 
-#[derive(DerefMut)]
 struct JobStatus {
     jobs_completed: u32,
 }
 
 fn main() {
     let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));    let status_shared = Arc::clone(&status);
+    let mut status_shared = status.clone();
     thread::spawn(move || {
         for _ in 0..10 {
-            thread::sleep(Duration::from_millis(250));  
-            status_shared.jobs_completed += 1;
+            thread::sleep(Duration::from_millis(250));
+            let mut unlocked_status = status_shared.lock().unwrap();
+            unlocked_status.jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+    while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
