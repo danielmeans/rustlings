fafc698ae5a664cec98f12a5a8f90e3c2b66a77f
Daniel Means <dmeans@cloudflare.com>
2023-01-25 21:54:57 -0800

    Code passes for try_from_into, but code is reptitive

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -36,6 +36,11 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        for color in [tuple.0, tuple.1, tuple.2] {
+            if color < 0 || color > 255 {
+                return Err(IntoColorError::IntConversion)
+            }
+        }
         return Ok(Color{
             red: tuple.0 as u8,
             green: tuple.1 as u8,
@@ -48,6 +53,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        for color in arr.iter() {
+            if color < &(0 as i16) || color > &(255 as i16) {
+                return Err(IntoColorError::IntConversion)
+            }
+        }
         return Ok(Color{
             red: arr[0] as u8,
             green: arr[1] as u8,
@@ -62,6 +72,11 @@ impl TryFrom<&[i16]> for Color {
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
         if slice.len() != 3 {
             return Err(IntoColorError::BadLen)
+        }
+        for color in slice.iter() {
+            if color < &(0 as i16) || color > &(255 as i16) {
+                return Err(IntoColorError::IntConversion)
+            }
         }
         return Ok(Color{
             red: slice[0] as u8,
